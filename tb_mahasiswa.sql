a173afd4811c59b898e6cbacb50cc5907d6a6160
Adrian Reyesta <muhammadadrianreyesta9d@gmail.com>
2022-08-11 20:25:11 +0700

    edit readme

---
+++ tb_mahasiswa.sql
@@ -0,0 +1,58 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 11 Agu 2022 pada 15.13
+-- Versi server: 10.4.24-MariaDB
+-- Versi PHP: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `crud`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_mahasiswa`
+--
+
+CREATE TABLE `tb_mahasiswa` (
+  `nim` varchar(25) NOT NULL,
+  `nama` varchar(250) NOT NULL,
+  `jurusan` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `tb_mahasiswa`
+--
+
+INSERT INTO `tb_mahasiswa` (`nim`, `nama`, `jurusan`) VALUES
+('091029', 'Umar', 'RPL'),
+('091029989', 'usmanw', 'RPL'),
+('2093098', 'AKNASA LALA', 'TKR');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `tb_mahasiswa`
+--
+ALTER TABLE `tb_mahasiswa`
+  ADD PRIMARY KEY (`nim`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
